3e84cbf151380a72a90c1084b8c08e937cc2943c
Brandon Choi <bchoi28@gmail.com>
2023-05-15 14:53:04 -0400

    on handleSubmit

---
+++ react-form-project/src/Form.jsx
@@ -0,0 +1,115 @@
+import { useState } from 'react';
+
+function Form(props) {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [phoneType, setPhoneType] = useState('');
+    const [staff, setStaff] = useState('');
+    const [bio, setBio] = useState('');
+    const [signedUp, setSignedUp] = useState(false);
+
+    const [errorMessages, setErrorMessages] = useState([]);
+
+    const validate = () => {
+        let errors = [];
+        const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        const phoneRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+
+        if (name.length === 0) {
+            errors.push("Name cannot be empty")
+        }
+        if (!email.match(emailRegex) || email.length === 0) {
+            errors.push("Must enter a valid email address")
+        }
+        if (phoneNumber && !phoneNumber.match(phoneRegex)) {
+            errors.push("Invalid phone number")
+        }
+        if (phoneNumber && !phoneType) {
+            errors.push("Must select phone type")
+        }
+        if (bio.length > 280) {
+            errors.push("Bio cannot have more than 280 characters")
+        }
+
+        return errors;
+    }
+
+    const handleChange = (field) => {
+        return (e) => {
+            switch (field) {
+                case "name":
+                    setName(e.target.value);
+                    break;
+                case "email":
+                    setEmail(e.target.value);
+                    break;
+                case "phoneNumber":
+                    setPhoneNumber(e.target.value);
+                    break;
+                case "phoneType":
+                    setPhoneType(e.target.value);
+                    break;
+                case "staff":
+                    setStaff(e.target.value);
+                    break;
+                case "bio":
+                    setBio(e.target.value);
+                    break;
+                case "signedUp":
+                    setSignedUp(e.target.checked);
+                    break;
+
+            }
+        }
+    }
+
+    const handleSubmit = () => {
+        const submittedData = { name, email, phoneNumber, phoneType, staff, bio, signedUp };
+        console.log(submittedData);
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Name" value={name} onChange={handleChange("name")} />
+            <br />
+
+            <input type="text" placeholder="Email" value={email} onChange={handleChange("email")} />
+            <br />
+
+            <input type="text" placeholder="Phone Number" value={phoneNumber} onChange={handleChange("phoneNumber")} />
+            <br />
+
+            <select value={phoneType} onChange={handleChange("phoneType")}>
+                <option disabled>Phone Number</option>
+                <option value="Home">Home</option>
+                <option value="Work">Work</option>
+                <option value="Mobile">Mobile</option>
+            </select>
+            <br />
+
+            <label>
+                <input type="radio" value="instructor" checked={staff === "instructor"} onChange={handleChange("staff")} />Instructor
+            </label>
+            <label>
+                <input type="radio" value="student" checked={staff === "student"} onChange={handleChange("staff")} />Student
+            </label>
+            <br />
+
+            <label>Bio:
+                <textarea placeholder="Bio" value={bio} onChange={handleChange("bio")} />
+            </label>
+            <br />
+
+            <label>Sign up for Email Notifications?
+                <input type="checkbox" value={signedUp} checked={signedUp} onChange={handleChange("signedUp")} />
+            </label>
+
+            <br />
+
+            <button>Submit</button>
+        </form>
+    )
+};
+
+export default Form;
