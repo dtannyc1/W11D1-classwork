da1f4e33728dfc199aafe33fa7664e9b847621c7
dtannyc1 <dtannyc1@gmail.com>
2023-05-15 16:54:05 -0400

    finished phase 3

--- practice-for-week-14-react-hooks-useEffect-long-practice-main/src/components/PictureDisplay.js
+++ practice-for-week-14-react-hooks-useEffect-long-practice-main/src/components/PictureDisplay.js
@@ -10,7 +10,7 @@ import feather8 from "../images/feather8.svg";
 import feather9 from "../images/feather9.svg";
 import featherA from "../images/featherA.svg";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const feathers = [
   feather1,
@@ -30,40 +30,59 @@ function PictureDisplay ({ size, featherCount, featherColors }) {
 //     console.log('PictureDisplay', size, featherCount, featherColors);
 //   }, [size, featherCount, featherColors]);
 
-useEffect(() => {
-    console.log('PictureDisplay size', size);
-}, [size]);
+    const [sizeClass, setSizeClass] = useState('');
 
-useEffect(() => {
-    console.log('PictureDisplay feather count', featherCount);
-}, [featherCount]);
+    useEffect(() => {
+        // console.log('PictureDisplay size', size);
+        let cname = '';
+        switch (size) {
+            case 'm':
+                cname = 'medium';
+                break;
+            case 'l':
+                cname = 'large';
+                break;
+            case 'xl':
+                cname = 'xlarge';
+                break;
+            default:
+                cname = 'small';
+                break;
+        }
+        setSizeClass(cname);
+        // console.log(cname);
+    }, [size]);
 
-useEffect(() => {
-    console.log('PictureDisplay feather colors', featherColors);
-}, [featherColors]);
+    useEffect(() => {
+        console.log('PictureDisplay feather count', featherCount);
+    }, [featherCount]);
+
+    useEffect(() => {
+        console.log('PictureDisplay feather colors', featherColors);
+    }, [featherColors]);
 
 
-  // TODO: Wrap in useEffect
-  const colors = [];
-  if (!featherColors || featherColors.length === 0) featherColors = [''];
-  for (let i=0; i<featherCount; i++) {
-    colors.push(featherColors[i % featherColors.length]);
-  }
+    // TODO: Wrap in useEffect
+    const colors = [];
+    if (!featherColors || featherColors.length === 0) featherColors = [''];
+    for (let i=0; i<featherCount; i++) {
+        colors.push(featherColors[i % featherColors.length]);
+    }
 
-  return (
-    <div className={`image-area medium`}>
-      {colors.map((c, i) =>
-        <img
-          key={feathers[i]}
-          src={feathers[i]}
-          className={`image-feather ${c}`}
-          alt=""
-        />
-      )}
+    return (
+        <div className={`image-area ${sizeClass}`}>
+            {colors.map((c, i) =>
+                <img
+                key={feathers[i]}
+                src={feathers[i]}
+                className={`image-feather ${c}`}
+                alt=""
+                />
+            )}
 
-      <img src={turkey} className="image-turkey" alt="turkey" />
-    </div>
-  );
+            <img src={turkey} className="image-turkey" alt="turkey" />
+        </div>
+    );
 }
 
 export default PictureDisplay;
